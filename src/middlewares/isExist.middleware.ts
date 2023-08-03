fb1479677d9efbe5251799c8dc50a9d4e50db0d9
NotVadusha <vadimkbondarchuk@gmail.com>
2023-08-03 13:00:19 +0300

    feat:add middleware to check if note exist

---
+++ src/middlewares/isExist.middleware.ts
@@ -0,0 +1,27 @@
+import { Response, NextFunction } from "express";
+import notesService from "../repositories/notes.repository";
+import { HttpError } from "../helpers/errors";
+import { TypedRequestParams } from "../Interfaces/IRequest";
+import { StatusCodes } from "http-status-codes";
+
+const isExistMiddleware = async (
+  req: TypedRequestParams<{ id: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const note = await notesService.getOne(req.params.id);
+    if (!note) {
+      throw new HttpError({
+        name: "Not found",
+        httpCode: StatusCodes.NOT_FOUND,
+        message: "Can't find note with such id",
+      });
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { isExistMiddleware };
